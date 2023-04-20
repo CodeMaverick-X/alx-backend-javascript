587c427439deab41b1b5330cc0edbc90c0f0ad47
reinhard <reinhard2057@gmail.com>
2023-04-20 15:24:04 +0000

    task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,16 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default function handleProfileSignup() {
+  const promise1 = createUser();
+  const promise2 = uploadPhoto();
+
+  const arr = [promise1, promise2];
+  const promise3 = Promise.all(arr);
+
+  promise3.then((results) => {
+    const { firstName } = results[0];
+    const { lastName } = results[0];
+    const { body } = results[1];
+    console.log(body, firstName, lastName);
+  });
+}
