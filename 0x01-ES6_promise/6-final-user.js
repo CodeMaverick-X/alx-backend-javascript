5a0bd9b09043e47a470b9c71f1c608bf13dcf187
reinhard <reinhard2057@gmail.com>
2023-04-23 11:03:03 +0000

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -7,5 +7,9 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
 
   const promise3 = Promise.allSettled([promise1, promise2]);
   const test = await promise3.then((results) => results);
+
+  const { message } = test[1].reason;
+  delete (test[1].reason);
+  test[1].value = message;
   return test;
 }
