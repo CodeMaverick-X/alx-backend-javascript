3a2bcbe5b24929abb9ec457ccbc1090a58a0cda2
reinhard <reinhard2057@gmail.com>
2023-04-23 10:16:25 +0000

    task 6 again

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,12 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
 
   const promise3 = Promise.allSettled([promise1, promise2]);
-  const test = promise3.then((results) => results);
-  return promise3
+  const test = await promise3.then((results) => results);
+  console.log(test);
+  return test;
 }
