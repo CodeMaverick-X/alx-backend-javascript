30da2ab244c4dabd7ca7198fcb2a57cd2d9bbc45
reinhard <reinhard2057@gmail.com>
2023-04-24 18:04:35 +0000

    task 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -31,7 +31,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this.amount}  ${this.currency.name} (${this.currency.code})`;
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
